43176349b6cdc24e0a84e2da32916b747360f14f
OpenShift guest <vymiyai@gmail.com>
2013-06-13 23:11:16 -0400

    Tornando o fornecimento de profiles um servico.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,17 +6,13 @@ angular.module('myApp.controllers', []).
     controller('MainController', [ '$scope', function($scope) {
         $scope.quit = function(){ alert("Exit.") };
     }])
-    .controller('NewGameController', [ '$scope', function($scope) {
-        $scope.profiles = [ {'name':'profile 1', 'value':'1'},
-                            {'name':'profile 2', 'value':'2'},
-                            {'name':'profile 3', 'value':'3'} ];
+    .controller('NewGameController', [ '$scope', 'profiles', function($scope, profiles) {
+        $scope.profiles = profiles;
         
         $scope.click = function( profileValue ){ alert( "Will overwrite profile number " + profileValue ); };
     }])
-    .controller('LoadProfileController', [ '$scope', function($scope) {
-        $scope.profiles = [ {'name':'profile 1', 'value':'1'},
-                            {'name':'profile 2', 'value':'2'},
-                            {'name':'profile 3', 'value':'3'} ];
+    .controller('LoadProfileController', [ '$scope', 'profiles', function($scope, profiles) {
+        $scope.profiles = profiles;
             
         $scope.click = function( profileValue ){ alert( "Will load profile number " + profileValue ); };
     }]);
--- app/js/services.js
+++ app/js/services.js
@@ -11,4 +11,9 @@ angular.module('myApp.services', []).
     value('version2', '0.2').
     service('version3', function(){
         return {"wololo":"wololo"};
+    }).
+    service('profiles', function(){
+        return [    {'name':'profile 1', 'value':'1'},
+                    {'name':'profile 2', 'value':'2'},
+                    {'name':'profile 3', 'value':'3'} ];
     });
