03f3e53b2f46d90ba480b14331fad33ae39dae9b
OpenShift guest <vymiyai@gmail.com>
2013-06-13 23:37:14 -0400

    Criando o botao de voltar usando data binding.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,17 +2,17 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
-    controller('MainController', [ '$scope', function($scope) {
+angular.module('myApp.controllers', [])
+    .controller('MainController', [ '$scope', function($scope) {
         $scope.quit = function(){ alert("Exit.") };
     }])
     .controller('NewGameController', [ '$scope', 'profiles', function($scope, profiles) {
         $scope.profiles = profiles;
-        
         $scope.click = function( profileValue ){ alert( "Will overwrite profile number " + profileValue ); };
+        $scope.bottomButton = {"link":"#/main", "label":"Back"};
     }])
     .controller('LoadProfileController', [ '$scope', 'profiles', function($scope, profiles) {
         $scope.profiles = profiles;
-            
         $scope.click = function( profileValue ){ alert( "Will load profile number " + profileValue ); };
+        $scope.bottomButton = {"link":"#/main", "label":"Back"};
     }]);
