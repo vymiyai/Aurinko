e8da372b54d5f9dfc2123b4845165df9bb9b1fde
OpenShift guest <vymiyai@gmail.com>
2013-06-16 23:35:47 -0400

    Adicionando servico de manipulacao de localStorage.

--- app/js/services.js
+++ app/js/services.js
@@ -16,10 +16,21 @@ angular.module('myApp.services', []).
                     {'name':'profile 2', 'value':'2'},
                     {'name':'profile 3', 'value':'3'} ];
     }).
-    service('ProfileHandler', function(){
-        return {    "loadProfile" : function(id)
+    service( 'ProfileHandler', function(){
+        // o unico profile que precisa estar disponivel em scope eh o que sera usado durante o jogo...
+        return {    "loadProfile" : function( id )
                     { 
-                        alert( "loading profile " + id ); 
-                    } 
+                        var stringifiedProfileData = window.localStorage.getItem( id );
+                        return JSON.parse( stringifiedProfileData );
+                    },
+                    "saveProfile" : function( id, profileData )
+                    {
+                        var stringifiedProfileData =  JSON.stringify( profileData );
+                        window.localStorage.setItem( id, stringifiedProfileData );
+                    },
+                    "clearProfiles" : function()
+                    {
+                        window.localStorage.clear();
+                    }
                 };
     });
