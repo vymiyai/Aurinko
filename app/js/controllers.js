08cf27d82932d4f146cc4fe56ea4c4b0363587a4
OpenShift guest <vymiyai@gmail.com>
2013-07-08 00:58:48 -0400

    Animacao de fade in e fade out funcionais.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,13 +3,15 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-    .controller('MainMenuController', [ '$scope', function($scope) {
+    .controller('MainMenuController', [ '$scope', '$document', function($scope, $document) {
         // define buttons for the menu.
         $scope.buttons = [  { value:"New Game",   href:"#/new_game" },
                             { value:"Load Game",  href:"#/load_game" },
                             { value:"Quit",       href:"#" } ];
                             
-        $scope.wololo = true;
+        $scope.wololo = false;
+        
+        $scope.trololol = function (){ $scope.wololo = true; };
     }])
     .controller('NewGameController', [ '$scope', "Profiles", "Profile", function( $scope, profiles, profile ) {
         $scope.profiles = profiles;
